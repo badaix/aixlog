8e8d884ee9364ca50546cbc7533d5f03c440ce81
badaix <johannes.pohl@badaix.de>
2020-04-21 10:08:45 +0200

    Remove "type", add log filters, Windows fixes
    - AixLog::Type is removed along with the SLOG macro.
    - Log sinks have filters, superseding the severity
    - Add SinkNull, discarding all log messages
    - Windows fixes

--- aixlog_example.cpp
+++ aixlog_example.cpp
@@ -19,25 +19,23 @@ using namespace std;
 
 int main(int /*argc*/, char** /*argv*/)
 {
-	AixLog::Log::init<AixLog::SinkCout>(AixLog::Severity::trace, AixLog::Type::normal);
+	AixLog::Log::init<AixLog::SinkCout>(AixLog::Severity::trace);
 	LOG(INFO) << "Logger with one cout log sink\n";
 
 	AixLog::Log::init(
 		{
 			/// Log everything into file "all.log"
-			make_shared<AixLog::SinkFile>(AixLog::Severity::trace, AixLog::Type::all, "all.log"),
-			/// Log normal (i.e. non-special) logs to SinkCout
-			make_shared<AixLog::SinkCout>(AixLog::Severity::trace, AixLog::Type::normal, "cout: %Y-%m-%d %H-%M-%S.#ms [#severity] (#tag_func) #message"),
+			make_shared<AixLog::SinkFile>(AixLog::Severity::trace, "all.log"),
+			/// Log everything to SinkCout
+			make_shared<AixLog::SinkCout>(AixLog::Severity::trace, "cout: %Y-%m-%d %H-%M-%S.#ms [#severity] (#tag_func) #message"),
 			/// Log error and higher severity messages to cerr
-			make_shared<AixLog::SinkCerr>(AixLog::Severity::error, AixLog::Type::all, "cerr: %Y-%m-%d %H-%M-%S.#ms [#severity] (#tag_func)"),
-			/// Log special logs to native log (Syslog on Linux, Android Log on Android, EventLog on Windows, Unified logging on Apple)
-			make_shared<AixLog::SinkNative>("aixlog", AixLog::Severity::trace, AixLog::Type::special),
+			make_shared<AixLog::SinkCerr>(AixLog::Severity::error, "cerr: %Y-%m-%d %H-%M-%S.#ms [#severity] (#tag_func)"),
 			/// Callback log sink with cout logging in a lambda function
 			/// Could also do file logging
-			make_shared<AixLog::SinkCallback>(AixLog::Severity::trace, AixLog::Type::all, 
+			make_shared<AixLog::SinkCallback>(AixLog::Severity::trace, 
 				[](const AixLog::Metadata& metadata, const std::string& message)
 				{
-					cout << "Callback:\n\tmsg:   " << message << "\n\ttag:   " << metadata.tag.text << "\n\tsever: " << AixLog::Log::to_string(metadata.severity) << " (" << static_cast<int>(metadata.severity) << ")\n\ttype:  " << (metadata.type == AixLog::Type::normal?"normal":"special") << "\n";
+					cout << "Callback:\n\tmsg:   " << message << "\n\ttag:   " << metadata.tag.text << "\n\tsever: " << AixLog::to_string(metadata.severity) << " (" << static_cast<int>(metadata.severity) << ")\n";
 					if (metadata.timestamp)
 						cout << "\ttime:  " << metadata.timestamp.to_string() << "\n";
 					if (metadata.function)
@@ -46,8 +44,6 @@ int main(int /*argc*/, char** /*argv*/)
 			)
 		}
 	);
-	/// Log special logs into file "special.log"
-	AixLog::Log::instance().add_logsink<AixLog::SinkFile>(AixLog::Severity::trace, AixLog::Type::special, "special.log");
 
 	/// Log with info severity
 	LOG(INFO) << "LOG(INFO)\n";
@@ -55,14 +51,6 @@ int main(int /*argc*/, char** /*argv*/)
 	LOG(INFO, "guten tag") << "LOG(INFO, \"guten tag\")\n";
 	/// ... with an explicit tag (same result as above)
 	LOG(INFO) << TAG("guten tag") << "LOG(INFO) << TAG(\"guten tag\")\n";
-	/// Log "special" with info severity
-	SLOG(INFO) << "SLOG(INFO)\n";
-	/// Log with explicit "special" type
-	LOG(INFO) << AixLog::Type::special << "LOG(INFO) << AixLog::Type::special\n";
-	/// Log with explicit "special" type (now with a macro)
-	LOG(INFO) << SPECIAL << "LOG(INFO) << SPECIAL\n";
-	/// ... with explicit "special" type and explicit tag
-	LOG(INFO) << SPECIAL << TAG("guten tag") << "LOG(INFO) << SPECIAL << TAG(\"guten tag\")\n";
 
 	/// Different log severities
 	LOG(FATAL) << "LOG(FATAL)\nLOG(FATAL) Second line\n";
