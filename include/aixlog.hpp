323b9ba707c413374bcfffa887ad35384531b025
badaix <johannes.pohl@badaix.de>
2021-02-23 21:27:29 +0100

    Raise version number to v1.5.0

--- include/aixlog.hpp
+++ include/aixlog.hpp
@@ -3,7 +3,7 @@
      / _\ (  )( \/ )(  )   /  \  / __)
     /    \ )(  )  ( / (_/\(  O )( (_ \
     \_/\_/(__)(_/\_)\____/ \__/  \___/
-    version 1.4.0
+    version 1.5.0
     https://github.com/badaix/aixlog
 
     This file is part of aixlog
